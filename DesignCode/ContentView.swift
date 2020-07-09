51fdbf310f6c9931a0162229fe4e027c36f9590d
Vong <Vong9262@gmail.com>
2020-07-10 00:22:54 +0800

    Initial Commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Vong on 2020/7/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Vong on 2020/7/10.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
