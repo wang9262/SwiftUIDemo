970be33d3996c7b342b972c64f6f22dc61594c90
Vong <Vong9262@gmail.com>
2020-07-11 10:53:28 +0800

    Section3-Animations and states

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -8,42 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show: Bool = false
+    
     var body: some View {
         ZStack {
             TitleView()
-                .blur(radius: 20)
-            
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
             
             BackCardView()
-                .background(Color("card4"))
+                .background(show ? Color("card3") : Color("card4"))
                 // 修饰符的前后顺序需要考虑，比如这里的阴影和圆角
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -40)
+                .offset(x: 0, y: show ? -400 : -40)
                 .scaleEffect(0.9)
-                .rotationEffect(.degrees(10))
+                .rotationEffect(.degrees(show ? 0 : 10))
                 .rotation3DEffect(
                     .degrees(10), axis: (x: 10, y: 0, z: 0)
                 )
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.35))
             
             BackCardView()
-                .background(Color("card3"))
+                .background(show ? Color("card4") : Color("card3"))
                 .cornerRadius(20)
                 .shadow(radius: 20)
-                .offset(x: 0, y: -20)
+                .offset(x: 0, y: show ? -200 : -20)
                 .scaleEffect(0.95)
-                .rotationEffect(.degrees(5))
+                .rotationEffect(.degrees(show ? 0 : 5))
                 .rotation3DEffect(
                     .degrees(5), axis: (x: 10, y: 0, z: 0)
                 )
                 .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.2))
             
             CardView()
                 .blendMode(.hardLight)
+                .onTapGesture {
+                    self.show.toggle()
+                }
             
             BottomCardView()
-                .blur(radius: 20)
+                .blur(radius: show ? 20 : 0)
+                .animation(.default)
         }
     }
 }
@@ -130,6 +138,6 @@ struct BottomCardView: View {
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(x: 0, y: 500)
+        .offset(x: 0, y: 550)
     }
 }
